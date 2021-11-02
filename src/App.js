9956b918d3b3b2eb72dc7649cba2ae6c83aa1431
climb719 <mary.oldweiler@gmail.com>
2021-11-02 12:25:56 -0400

    add enter button to test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import EnterButton from './components/EnterButton'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>
           Welcome to Swatched
         </h1>
+        <EnterButton />
 
       </header>
     </div>
---
+++ src/components/EnterButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+const EnterButton = () => {
+    return <button >
+            Get Swatched
+          </button>
+}
+
+
+export default EnterButton
