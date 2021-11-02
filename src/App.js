efeddae59c1c96579077adb89db33053b83cda8a
climb719 <mary.oldweiler@gmail.com>
2021-11-02 12:54:08 -0400

    test complete add image/css change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://i.pinimg.com/564x/1e/db/25/1edb253a19ebb84b7528e9c33e375159.jpg" className="home-panels" alt="swatches" />
         <h1>
           Welcome to Swatched
         </h1>
