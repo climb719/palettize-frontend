2c229f0988302929600170528bc68a32e6c7d989
climb719 <mary.oldweiler@gmail.com>
2021-11-03 11:52:35 -0400

    finish setting up thunk with compose/redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { paletteReducer } from './redux/paletteReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
-const store = createStore(paletteReducer, applyMiddleware(thunk))
-//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(paletteReducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -17,9 +18,9 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
