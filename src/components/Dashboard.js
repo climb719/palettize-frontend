686063fabed87059f7f8434a19d66f3b64293921
climb719 <mary.oldweiler@gmail.com>
2021-11-05 13:09:52 -0400

    set up dashboard component, css

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,13 @@
 const Dashboard = props => {
     console.log(props)
   return (
-    <div className="App-Main">
-    <h2>Glad you came back, Login here:</h2>
+    <div className="dashboard">
+      <div className="dashboard-main">
+        <h2 className="my_palettes">My Palettes</h2>
+    <h4 className="login">Glad you came back <br></br> Login here:</h4>
+    <p className="my_info"> View My Profile </p>
+  
+    </div>
     </div>
   )
 
