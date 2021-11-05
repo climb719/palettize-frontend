17206563f3749c027545528016954f4020858697
climb719 <mary.oldweiler@gmail.com>
2021-11-05 15:11:20 -0400

    test url input field and submit options

--- src/components/Create.js
+++ src/components/Create.js
@@ -4,13 +4,31 @@ import { ColorExtractor } from 'react-color-extractor'
 import { test } from '../redux/actionCreators'
 
 
-const Create = (props) => {
+class Create extends React.Component {
 
+    handleClick = (e) => {
+        e.preventDefault()
+        this.props.test()
+    }
+
+    render() {
     return (
         <div>
-            <button onClick={props.test}>Create</button>
+            <ColorExtractor >
+            <img className="image_field"
+            src="https://media.istockphoto.com/photos/blue-sky-with-rainbow-and-sun-reflection-picture-id518324936?b=1&k=20&m=518324936&s=170667a&w=0&h=3Rht3gTpVgB8oSAKvBmSthglJStzjkGgzTA28tlDRvU=" alt="your_input"
+         
+            />
+            </ColorExtractor>
+            <form>
+              <input type="text" name="image_url" placeholder="drop a url here..." size="50"/>&nbsp;
+              <input type="submit" value="Submit"  onClick={this.handleClick}/>
+            </form>
+        
         </div>
-    )
+        )
+    }
+
 }
 
 const mapStateToProps = (state) => {
@@ -18,5 +36,6 @@ const mapStateToProps = (state) => {
         colors: state.colors
     }
 }
+
 
 export default connect(mapStateToProps, {test})(Create)
