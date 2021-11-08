252f574cd6f04989dc65520d0068a88c7b63a9cd
climb719 <mary.oldweiler@gmail.com>
2021-11-08 09:45:29 -0500

    css play with signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -16,16 +16,28 @@ function Signup(props){
 
   return (
     <div className="App-Main">
-    <h2>Glad you want to Palettize, Sign Up here:</h2>
-    <form className="signup-form" onSubmit={handleSubmit}>
-      Username: <input type="text" name="username" value={user.username} onChange={handleChange}/> <br/>
-      Password: <input type="password" name="password" value={user.password} onChange={handleChange}/> <br/>
-      Password Confirmation: <input type="password" name="passwordConfirmation" value={user.passwordConfirmation} onChange={handleChange}/>
-      <br/>
-      <input type="submit" value="Submit" />
+     
+    <h2 className="signup-title">Palettize</h2>
+    <p className="signup-text">Bring some color to your life</p>
+    <div className="signup-form">
+    <form  onSubmit={handleSubmit}>
+    <p> <label>
+      Username 
+      </label>   
+      <input type="text" name="username" value={user.username} onChange={handleChange} size="40"/> </p>
+      <p> <label>
+        Password<br/>
+      </label>
+      <input type="password" name="password" value={user.password} onChange={handleChange} size="40" /></p>
+      <p> <label>
+        Password Confirmation<br/>
+      </label>
+      <input type="password" name="passwordConfirmation" value={user.passwordConfirmation} onChange={handleChange} size="40"/></p>
+      <p><input type="submit" value="Submit" /></p>
     </form>
     <br/>
-    <NavLink to="/Login">Or Login</NavLink>
+    Already on Palettize?<NavLink to="/Login"> Login</NavLink>
+    </div>
     </div>
   )
 
