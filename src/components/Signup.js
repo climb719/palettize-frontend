ef245ccf877f2653ab455c7d5c1b14afbe6a5cea
climb719 <mary.oldweiler@gmail.com>
2021-11-08 08:47:02 -0500

    sidnup on chnage working and submit console logging user

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,22 +1,26 @@
 import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
+import { connect } from 'react-redux'
 
 function Signup(props){
   const [user, setUser] = useState({username: "", password: "", passwordConfirmation: ""})
+  
   const handleChange = (e) => setUser({...user, [e.target.name]: e.target.value})
     console.log(props)
+    console.log(user)
   const handleSubmit = (e) => {
       e.preventDefault()
+      console.log(user)
       
   }
 
   return (
     <div className="App-Main">
     <h2>Glad you want to Palettize, Sign Up here:</h2>
-    <form onSubmit={handleSubmit}>
+    <form className="signup-form" onSubmit={handleSubmit}>
       Username: <input type="text" name="username" value={user.username} onChange={handleChange}/> <br/>
       Password: <input type="password" name="password" value={user.password} onChange={handleChange}/> <br/>
-      Password Confirmation: <input type="password" value={user.passwordConfirmation} onChange={handleChange}/>
+      Password Confirmation: <input type="password" name="passwordConfirmation" value={user.passwordConfirmation} onChange={handleChange}/>
       <br/>
       <input type="submit" value="Submit" />
     </form>
