b61b6c03e6ef5832e9ab03e2509e1a66c48d6636
climb719 <mary.oldweiler@gmail.com>
2021-11-10 10:54:12 -0500

    refactor App.js to function without getPalettes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import { Home, Create, Dashboard, Signup, Login, Nav } from './components'
 import { connect } from 'react-redux'
-// import { getPalettes } from './redux/actionCreators'
 import PaletteContainer from './containers/PalleteContainer'
 
 
-class App extends React.Component {
+function App(props) {
 
-  // componentDidMount(){
-  //   this.props.getPalettes()
-  // }
+  console.log(props)
 
- 
-
-  render(){ 
   return (
     <div className="App">
       <Nav />
       <Switch>
-      <Route path="/signup"  render={(routeProps) => <Signup {...routeProps} setAppUser={this.setAppUser}/>}/> 
-      <Route path="/login"  render={(routeProps) => <Login {...routeProps} setAppUser={this.setAppUser}/>}/> 
+      <Route path="/signup"  render={(routeProps) => <Signup {...routeProps} />}/> 
+      <Route path="/login"  render={(routeProps) => <Login {...routeProps} />}/> 
       <Route path='/dashboard' component={Dashboard} />
       <Route path='/palettes'component={PaletteContainer} />
       <Route path='/create' component={Create} />
@@ -29,17 +23,9 @@ class App extends React.Component {
       </Switch>
       </div>
    
-    );
-  }
+    )
+  
 }
-
-
-// function setAppUser(state) {
-//   const setAppUser = {setAppUser: state.user}
-//   console.log(setAppUser)
-//   }
-
-
 
 const mapStateToProps = state => {
   return {
