da9a94a9b5cb3ef100c891a6ed3dca1900dee6bd
climb719 <mary.oldweiler@gmail.com>
2021-11-11 09:39:43 -0500

    start filter for tag button click

--- src/components/TagButtons.js
+++ src/components/TagButtons.js
@@ -1,15 +1,25 @@
 import { connect } from 'react-redux'
-function TagButtons ({tags}) {
+
+function TagButtons ({tags, palettes}) {
   console.log({tags})
+    console.log({palettes})
+
+ const handleClick= (e) => {
+    console.log(e.target.innerText)
+   const filteredPalettes = palettes.filter(palette => palette.tags.includes(e.target.innerText))
+   console.log(filteredPalettes)
+  }
+
     return (
         <div className="tags-container">
-        {tags.map((tag, index) => <button className="tag-buttons" key={index}> {tag} </button>)}
+        {tags.map((tag, index) => <button onClick={handleClick} className="tag-buttons" key={index}> {tag} </button>)}
          </div>
     )
   }
   
   const mapStateToProps = (state) => {
     return {...state.selectedPalette,
+       palettes: state.palettes
     }
   }
 
