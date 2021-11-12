0a36cb3911593af37d864773d4ae9016ecf64863
climb719 <mary.oldweiler@gmail.com>
2021-11-12 06:36:57 -0500

    fix add text and css

--- src/components/AddPalette.js
+++ src/components/AddPalette.js
@@ -2,9 +2,9 @@ function AddPalette() {
 
     return (
         <div className="App-Main">
-        <h2 className="login-title">Palettize</h2>
-          <p className="login-text">Add A Palette Here</p>
-            <div className="login-form">
+        <h2 className="add-title">Palettize</h2>
+          <p className="add-text">Add A Palette Here</p>
+            <div className="add-form">
             </div>
     </div>
     )
