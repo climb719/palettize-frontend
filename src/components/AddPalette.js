346c21a8146c90678f467f1998e3dc545518ebc2
climb719 <mary.oldweiler@gmail.com>
2021-11-12 06:30:18 -0500

    start addPalette component and fix export

---
+++ src/components/AddPalette.js
@@ -0,0 +1,14 @@
+function AddPalette() {
+
+    return (
+        <div className="App-Main">
+        <h2 className="login-title">Palettize</h2>
+          <p className="login-text">Add A Palette Here</p>
+            <div className="login-form">
+            </div>
+    </div>
+    )
+    
+}
+
+export default AddPalette
