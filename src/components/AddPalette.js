8477d6c5e218b187947d1ab44cffb8b411adca0f
climb719 <mary.oldweiler@gmail.com>
2021-11-12 13:36:51 -0500

    testing addPalette options

--- src/components/AddPalette.js
+++ src/components/AddPalette.js
@@ -5,23 +5,16 @@ import Select from 'react-select'
 import makeAnimated from 'react-select/animated';
 import { tagOptions } from './data';
 
-
-
-
-
 function AddPalette ({paletteTags}) {
         //console.log(paletteTags)
     const [colors, setColors] = useState([])
+    const [optionSelected, setSelected] = useState("")
 
-    const allTags = paletteTags.flat()
-    const uniqueTags = [...new Set(allTags)].sort()
-    console.log(uniqueTags)
 
     const animatedSelect = makeAnimated()
 
-
     const handleChange = (e) => setColors({...colors, [e.target.name]: e.target.value}, console.log(colors))
-    
+    const handleSelectChange = (selected) => setSelected({optionSelected: selected}, console.log(selected))
 
     return (
         <div className="App-Main">
@@ -32,9 +25,9 @@ function AddPalette ({paletteTags}) {
             <p>Choose your palettes's colors:</p>
             <div>
             <p>Color 1: <input type="color" id="color_1" name="color_1" value={colors[0]} onChange={handleChange} /></p>
-            <p>Color 2: <input type="color" id="color_2" name="color_2" value={colors[1]} onChange={handleChange}/></p>
-            <p>Color 3: <input type="color" id="color_3" name="color_3" value={colors[2]} onChange={handleChange}/></p>
-            <p>Color 4: <input type="color" id="color_4" name="color_4" value={colors[3]} onChange={handleChange}/></p> 
+            <p>Color 2: <input type="color" id="color_2" name="color_2"  onChange={handleChange}/></p>
+            <p>Color 3: <input type="color" id="color_3" name="color_3"  onChange={handleChange}/></p>
+            <p>Color 4: <input type="color" id="color_4" name="color_4"  onChange={handleChange}/></p> 
             </div> 
             <Select
             closeMenuOnSelect={false}
@@ -42,6 +35,8 @@ function AddPalette ({paletteTags}) {
             defaultValue="Select your tags"
             isMulti
             options={tagOptions}
+            onChange={handleSelectChange}
+           
             />
             <p><input type="submit" value="Submit" /></p>
             </form>            
