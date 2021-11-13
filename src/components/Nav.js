947bd8887b3846e3b803150fa5b85f0de288efb0
climb719 <mary.oldweiler@gmail.com>
2021-11-13 14:15:43 -0500

    refactor nav depending on if user

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,12 +17,12 @@ function Nav(props){
   return <nav>
       <div className="nav-bar">
       <NavLink  className="nav-link" to="/palettes">Browse Palettes</NavLink>&nbsp;
-      <NavLink className="nav-link" to="/dashboard">My Dashboard</NavLink>&nbsp;
-      <NavLink className="nav-link" to="/login">Login</NavLink>&nbsp;
-      <NavLink className="nav-link" to="/signup">Sign up</NavLink>&nbsp;
-      <NavLink className="nav-link" to="/create">Create</NavLink>&nbsp;
-      <NavLink className="nav-link" to="/add">Add A Palette</NavLink>&nbsp;
-      <NavLink onClick={handleLogout} className="nav-link" to="/logout">Logout</NavLink>&nbsp;
+      {props.user && <NavLink className="nav-link" to="/add">Add A Palette</NavLink>}&nbsp;
+      <NavLink className="nav-link" to="/create">Create From Image</NavLink>&nbsp;
+      {props.user && <NavLink className="nav-link" to="/dashboard">My Dashboard</NavLink>}&nbsp;
+      {!props.user && <NavLink className="nav-link" to="/login">Login</NavLink>}&nbsp;
+      {!props.user && <NavLink className="nav-link" to="/signup">Sign up</NavLink>}&nbsp;
+      {props.user && <NavLink onClick={handleLogout} className="nav-link" to="/logout">Logout</NavLink>}&nbsp;
       </div>
   </nav>
 }
