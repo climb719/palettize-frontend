6fb8767299585d6e831f36b52c8c6ad6badd0a68
climb719 <mary.oldweiler@gmail.com>
2021-11-15 11:38:43 -0500

    getting form data into color and tag arrays

--- src/components/AddPalette.js
+++ src/components/AddPalette.js
@@ -9,25 +9,42 @@ function AddPalette (props) {
         //console.log(paletteTags)
     const [colors, setColors] = useState([])
     const [optionSelected, setSelected] = useState("")
+    // const [palette, setPalette] = useState({colors: [], tags: []})
 
 
     const animatedSelect = makeAnimated()
 
     const handleChange = (e) => setColors({...colors, [e.target.name]: e.target.value}, console.log(colors))
+    
     const handleSelectChange = (selected) => setSelected({optionSelected: selected}, console.log(optionSelected))
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let paletteArr = Object.values(colors)
+      
+        let tagArr = optionSelected.optionSelected.map(tag => tag.value)
+        console.log(tagArr)
+        if (paletteArr.length !== 4) {
+            alert("You have not selected 4 colors, make sure you have made your own selection for colors 1-4")
+        } else {
+        console.log(paletteArr)
+        }
+        
+       // debugger 
+    }
 
     return (
         <div className="App-Main">
         <h2 className="add-title">Palettize</h2>
           <p className="add-text">Add A Palette Here</p>
             <div className="add-form">
-            <form> 
+            <form onSubmit={handleSubmit}> 
             <p>Choose your palettes's colors:</p>
             <div>
-            <p>Color 1: <input type="color" id="color_1" name="color_1" value={colors[0]} onChange={handleChange} /></p>
-            <p>Color 2: <input type="color" id="color_2" name="color_2"  onChange={handleChange}/></p>
-            <p>Color 3: <input type="color" id="color_3" name="color_3"  onChange={handleChange}/></p>
-            <p>Color 4: <input type="color" id="color_4" name="color_4"  onChange={handleChange}/></p> 
+            <p>Color 1: <input required={true} type="color" id="color1" name="color1" value={colors[0]} onChange={handleChange} /></p>
+            <p>Color 2: <input type="color" id="color2" name="color2" value={colors[1]}  onChange={handleChange}/></p>
+            <p>Color 3: <input type="color" id="color3" name="color3" value={colors[2]}  onChange={handleChange}/></p>
+            <p>Color 4: <input type="color" id="color4" name="color4" value={colors[3]} onChange={handleChange}/></p> 
             </div> 
             <Select
             closeMenuOnSelect={false}
