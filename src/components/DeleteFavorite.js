4675a7559b3540f39dde7452d0bd56bc37e17586
climb719 <mary.oldweiler@gmail.com>
2021-11-30 08:55:19 -0500

    remove notes and console.logs from DeleteFavorite

--- src/components/DeleteFavorite.js
+++ src/components/DeleteFavorite.js
@@ -5,7 +5,7 @@ import {  useHistory } from 'react-router-dom'
 function DeleteFavorite ({user, id, deletePalette}) {
 
     const history = useHistory()
-
+    
     const userFavorites = user.favorites
     const favs = userFavorites.map(f => ({id: f.id, palette: f.palette.id} ))
     const fav = favs.find( ({palette}) => palette === parseInt(id) )
@@ -13,19 +13,10 @@ function DeleteFavorite ({user, id, deletePalette}) {
     const favoriteId = fav.id
 
     const handleDeleteClick = () => {
-        console.log(fav)
-        console.log(paletteId, "palette id")
-        console.log(favoriteId, "favorite id")
         deletePalette(paletteId, favoriteId, history)
     }
- 
     return <button onClick={handleDeleteClick}> Delete From My Favorites</button>
   }
-  
-
-//   const mapDispatchToProps = (dispatch, favoriteId) => ({
-//     deletePalette: paletteId => dispatch(deletePalette(paletteId, favoriteId))
-//   })
 
 export default connect(null, {deletePalette})(DeleteFavorite)
   
